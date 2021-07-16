cf371a04ac1ecbcb1102097bf64f5f83d0e78d78
tki2396 <ijosetobi@gmail.coms>
2021-07-15 23:52:16 -0500

    commented out post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,84 +1,91 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity} from 'react-native';
+// import React, { useState } from "react";
+// import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity} from 'react-native';
 
-type Props = {
-    title: string,
-    description: string,
-    image_url: string
-};
+// type Props = {
+//     userName: string,
+//     profilePicture: string,
+//     caption: string,
+//     liked: boolean,
+//     image: string
+// };
 
 
-const Post = (props: Props) => {
-    const [modalVisible, setModalVisible] = useState(false);
+// const Post = (props: Props) => {
+//     const [userName, setUserName] = useState('');
+//     const [profilePicture, setProfilePicture] = useState('');
+//     const [caption, setCaption] = useState('');
+//     const [liked, setLiked] = useState(false);
+//     const [image, setImage] = useState('');
 
-    return(
-        <TouchableOpacity style={styles.container}>
-            <View style={{flex: 1, flexDirection: 'column', marginBottom: 10}}>
+
+//     return(
+//         <TouchableOpacity style={styles.container}>
+//             <View style={{flex: 1, flexDirection: 'column', marginBottom: 10}}>
         
-                <Image style={styles.photo} source={{uri: props.image_url}}/>
-                <View style={styles.container_text}>
-                    <Text style={styles.title}>
-                        {props.title}
-                    </Text>
-                    <Text style={styles.description}>
-                        {props.description}
-                    </Text>
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
-}
+//                 <Image style={styles.photo} source={{uri: props.image_url}}/>
+//                 <View style={styles.container_text}>
+//                     <Text style={styles.title}>
+//                         {props.title}
+//                     </Text>
+//                     <Text style={styles.description}>
+//                         {props.description}
+//                     </Text>
+//                 </View>
+//             </View>
+//         </TouchableOpacity>
+//     );
+// }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        borderRadius:20,
-        elevation: 3,
-        backgroundColor: '#FFF',
-        shadowOffset: {
-            width: 1,
-            height: 1
-        },
-        shadowColor: '#333',
-        shadowOpacity: 0.3,
-        shadowRadius: 2,
-        marginHorizontal: 4,
-        height: 350,
-        width: "98%",
-        justifyContent: 'center',
-        marginBottom: 10,
-    },
-    title: {
-        fontSize: 16,
-        color: '#000',
-        fontWeight: 'bold',
-        marginTop: 30,
-        textAlign: 'center'
-    },
-    container_text: {
-        flex: 1,
-        flexDirection: 'column',
-        marginLeft: 12,
-        justifyContent: 'center',
-    },
-    description: {
-        marginTop: 10,
-        fontSize: 11,
-        fontStyle: 'italic',
-    },
-    photo: {
-        height: 200,
-        width: 300,
-        flex: 1,
-        alignSelf:'center',
-        borderRadius: 15,
-        marginTop: 15
-    },
-    icon: {
-        marginRight: 50,
-        paddingLeft: 10,
-        alignSelf: 'center'
-    },
-});
-export default Post;
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         flexDirection: 'row',
+//         borderRadius:20,
+//         elevation: 3,
+//         backgroundColor: '#FFF',
+//         shadowOffset: {
+//             width: 1,
+//             height: 1
+//         },
+//         shadowColor: '#333',
+//         shadowOpacity: 0.3,
+//         shadowRadius: 2,
+//         marginHorizontal: 4,
+//         height: 350,
+//         width: "98%",
+//         justifyContent: 'center',
+//         marginBottom: 10,
+//     },
+//     title: {
+//         fontSize: 16,
+//         color: '#000',
+//         fontWeight: 'bold',
+//         marginTop: 30,
+//         textAlign: 'center'
+//     },
+//     container_text: {
+//         flex: 1,
+//         flexDirection: 'column',
+//         marginLeft: 12,
+//         justifyContent: 'center',
+//     },
+//     description: {
+//         marginTop: 10,
+//         fontSize: 11,
+//         fontStyle: 'italic',
+//     },
+//     photo: {
+//         height: 200,
+//         width: 300,
+//         flex: 1,
+//         alignSelf:'center',
+//         borderRadius: 15,
+//         marginTop: 15
+//     },
+//     icon: {
+//         marginRight: 50,
+//         paddingLeft: 10,
+//         alignSelf: 'center'
+//     },
+// });
+// export default Post;
